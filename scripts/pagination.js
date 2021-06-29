45681ffe888457f453a75cee1138a03530bbfc5d
joodavi <joodavi@live.com>
2021-06-29 18:09:51 -0300

    Paginação

---
+++ scripts/pagination.js
@@ -0,0 +1,134 @@
+var data = document.querySelectorAll('#paginate .list .item')
+data = Array.from(data)
+
+const html = {
+  get(element) {
+    return document.querySelector(element)
+  }
+}
+
+let perPage = 15
+const state = {
+  page: 1,
+  perPage,
+  totalPages: Math.ceil(data.length / perPage),
+  maxVisibleButtons: 5
+}
+
+const controls = {
+  next() {
+    state.page++
+
+    const lastPage = state.page > state.totalPages
+    if(lastPage) {
+      state.page--
+    }
+  },
+  prev() {
+    state.page--
+
+    if (state.page < 1) {
+      state.page++
+    }
+  },
+  goTo(page) {
+    if (page < 1) {
+      page = 1
+    }
+
+    state.page = +page
+
+    if (page > state.totalPages) {
+      state.page = state.totalPages
+    }
+  },
+  createListeners() {
+    html.get('.prev').addEventListener('click', () => {
+      controls.prev()
+      update()
+    })
+
+    html.get('.next').addEventListener('click', () => {
+      controls.next()
+      update()
+    })
+  }
+}
+
+const list = {
+  create(item) {
+    html.get('.list').appendChild(item)
+  },
+  update() {
+    html.get('.list').innerHTML = ""
+
+    let page = state.page - 1
+    let start = page * state.perPage
+    let end = start + state.perPage
+    
+    const paginatedItems = data.slice(start, end)
+
+    paginatedItems.forEach(list.create)
+  }
+}
+
+const buttons = {
+  element: html.get('.pagination .numbers'),
+  create(number) {
+    const button = document.createElement('div')
+
+    button.innerHTML = number;
+
+    if (state.page == number) {
+      button.classList.add('active')
+    }
+
+    button.addEventListener('click', (e) => {
+      const page = e.target.innerText
+
+      controls.goTo(page)
+      update()
+    })
+
+    buttons.element.appendChild(button)
+  },
+  update() {
+    buttons.element.innerHTML = ""
+    const { maxLeft, maxRight } = buttons.calculateMaxVisible()
+
+    for (let page = maxLeft; page <= maxRight; page++) {
+      buttons.create(page)
+    }
+  },
+  calculateMaxVisible() {
+    const { maxVisibleButtons } = state
+    let maxLeft = (state.page - Math.floor(state.maxVisibleButtons/2))
+    let maxRight = (state.page + Math.floor(maxVisibleButtons/2))
+
+    if (maxLeft < 1) {
+      maxLeft = 1
+      maxRight = maxVisibleButtons
+    }
+
+    if (maxRight > state.totalPages) {
+      maxLeft = state.totalPages - (maxVisibleButtons-1)
+      maxRight = state.totalPages
+
+      if(maxLeft<1) maxLeft = 1 
+    }
+
+    return { maxLeft, maxRight }
+  }
+}
+
+function update() {
+  list.update()
+  buttons.update()
+}
+
+function init() {
+  update()
+  controls.createListeners()
+}
+
+init()
