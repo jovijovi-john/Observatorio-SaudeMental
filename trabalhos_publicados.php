85366337a7c0c86516da8df74db93d03647458c9
joodavi <joodavi@live.com>
2021-06-30 08:29:39 -0300

    Correções

--- trabalhos_publicados.php
+++ trabalhos_publicados.php
@@ -10,7 +10,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Observatório</title>
 
-  <link rel="icon" href="./assets/images/LogoObservatorio.png">
+  <link rel="icon" href="./assets/images/LogoObservatorioBranca.png">
 
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
   <link rel="preconnect" href="https://fonts.gstatic.com">
