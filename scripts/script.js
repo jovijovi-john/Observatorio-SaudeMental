0cf6bea2b3e3adf0b8105a7cf335b458bbfd8e78
joodavi <joodavi@live.com>
2021-07-07 13:46:56 -0300

    Mudanças na página de Trabalhos

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,3 @@
-function DropdownSobre() {
-  document.getElementById("dropdown-sobre").classList.toggle("show-dropdown-sobre");
-
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content-sobre");
-      var i;
-      for (i=0; i<dropdowns.length; i++) {
-        var openDropdowns = dropdowns[i];
-        if (openDropdowns.classList.contains('show-dropdown-sobre')) {
-          openDropdowns.classList.remove('show-dropdown-sobre');
-        }
-      }
-    }
-  }
-}
-
-function DropdownProdutos() {
-  document.getElementById("dropdown-produtos").classList.toggle("show-dropdown-produto");
-
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content-produtos");
-      var i;
-      for (i=0; i<dropdowns.length; i++) {
-        var openDropdowns = dropdowns[i];
-        if (openDropdowns.classList.contains('show-dropdown-produto')) {
-          openDropdowns.classList.remove('show-dropdown-produto');
-        }
-      }
-    }
-  }
-}
-
 function openMenu() {
   document.getElementById("nav-bar").style.height = "100%";
 }
@@ -64,4 +30,5 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   time = setTimeout(showSlides, 4000);
 }
+
 
