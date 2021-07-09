1fa969fbee36862556325cdec99327eb94831dc3
ArthurPassos16 <arthurovitalio@gmail.com>
2021-07-09 10:27:26 -0300

    atualizações no menu

--- trabalhos_publicados.php
+++ trabalhos_publicados.php
@@ -8,7 +8,9 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Observatório Saúde Mental</title>
+
+  <link rel="icon" href="./assets/images/LogoObservatorio2.png">
   
   <link rel="stylesheet" href="./styles/trabalhos_publicados.css">
   <link rel="stylesheet" href="./styles/styles.css">
