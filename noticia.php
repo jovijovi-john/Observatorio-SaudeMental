d03cd8318595d3e2d3286ae40d7dfa77eb715b59
Matheus Levy <levybessa2001@hotmail.com>
2021-07-09 17:03:21 -0300

    att noticia

--- noticia.php
+++ noticia.php
@@ -1,22 +1,22 @@
 <?php
 	require_once('conexao.php');
-            mysqli_select_db($mysqli, $bd) or die("Could not select database");	
-            $stmt = $mysqli->prepare('SELECT * FROM noticias WHERE idNoticia =  ?');
-            $stmt->bind_param('i', $_GET['Noticia']);
-            $stmt->execute();
+    mysqli_select_db($mysqli, $bd) or die("Could not select database");	
+    $stmt = $mysqli->prepare('SELECT * FROM noticias WHERE idNoticia =  ?');
+    $stmt->bind_param('i', $_GET['Noticia']);
+    $stmt->execute();
 
-            $result = $stmt->get_result();
+    $result = $stmt->get_result();
 
-            $row = $result->fetch_assoc();
+    $row = $result->fetch_assoc();
 
+    $num_results = mysqli_num_rows($result);
+    if($num_results > 0 ){
 ?>
 
   <main>
     <section class="container">
       <div class="image">
         <img src="./assets/noticias/<?php print_r(utf8_encode($row['Foto'])) ?>" alt="">
-        <p>Mão com pílulas e remédios</p>
-        <span>Crédito Imagem</span>
       </div>
       <div class="texto-noticia">
         <h1><?php print_r(utf8_encode($row['Titulo'])) ?></h1>
@@ -24,3 +24,10 @@
       </div>
     </section>
   </main>
+
+ <?php
+}else{
+	header("location: ListaNoticias.php");
+	die();
+	}
+?>
