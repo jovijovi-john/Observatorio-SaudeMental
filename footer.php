ecb4d71227f2a860b1f77a8eeabdde301fb4a2a4
joodavi <joodavi@live.com>
2021-07-18 20:53:00 -0300

    att footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer>
-    <div class="container">
-      <div class="logos">
-        <a href="./index.html"><img src="./assets/images/LogoObservatorioBranca.png" alt=""></a>
+    <div class="container-logos-footer">  
+      <div class="logos-footer">
+        <a href="./index.html" class="logo-observatorio"><img src="./assets/images/LogoObservatórioBranca.png" alt=""></a>
         <div class="logos-secundarias">
           <a href="http://saude.sobral.ce.gov.br/politicas-sobre-drogas"><img src="./assets/images/logo-coord-psds.png" alt=""></a>
           <a href=""><img src="./assets/images/logo-gesam-sem-fundo.png" alt=""></a>
@@ -9,21 +9,24 @@
           <a href="http://www.uvanet.br"><img src="./assets/images/logo-uva-sem-texto.png" alt=""></a>
         </div>
       </div>
-      <div class="infos">
-        <ul>
-          <li><img src="./assets/svg/email icon.svg" alt=""><p>atendimento@observatoriodesaudemental.com.br</p></li>
-          <li><img src="./assets/svg/email icon.svg" alt=""><p>cpdsobral2017@gmail.com</p></li>
-        </ul>
-      </div>
-      <div class="sociais">
-        <ul>
-          <li><img src="./assets/svg/phone_black_24dp.svg" alt="">(88)993584258</li>
-          <li><img src="./assets/svg/phone_black_24dp.svg" alt="">(88)36111164</li>
-        </ul>
-      </div>
-      <div class="contato">
-        <img src="./assets/svg/comment icon.svg" alt=""><a href="./contato.php">Fale Conosco</a>
+    </div>
+    <div class="infos">
+      <h4>Contatos</h4>
+      <ul>
+        <li><img src="./assets/svg/email icon.svg" alt=""><p>atendimento@observatoriodesaudemental.com.br</p></li>
+        <li><img src="./assets/svg/email icon.svg" alt=""><p>cpdsobral2017@gmail.com</p></li>
+        <li><img src="./assets/svg/phone_black_24dp.svg" alt=""><p>(88)993584258</p></li>
+        <li><img src="./assets/svg/phone_black_24dp.svg" alt=""><p>(88)36111164</p></li>
+      </ul>
+    </div>
+    
+    <div class="container-fale-conosco">
+      <div class="contatos">
+        <div class="container-contato">
+          <div class="contato">
+            <img src="./assets/svg/comment icon.svg" alt=""><a href="./contato.html">Fale Conosco</a>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</footer>
+  </footer>
