d007b61cc2509aabe214c38145b881d216a2748f
MatheusLevy <48130978+MatheusLevy@users.noreply.github.com>
2021-07-19 00:45:42 -0300

    att ocultado servidor smtp

--- email.php
+++ email.php
@@ -10,13 +10,6 @@ use PHPMailer\PHPMailer\Exception;
 if (!isset($_POST['nome']) or !isset($_POST['email']) or !isset($_POST['mensagem'])) {
 	echo "Pacote não Permitido";
 }else{
-
-	/*
-	email:contatoteste@observatoriodesaudemental.com.br
-	senha: $Teste$Contato$22
-
-	*/
-
 	
 
 	function email($para_email, $para_nome,$assunto, $body){
@@ -24,10 +17,10 @@ if (!isset($_POST['nome']) or !isset($_POST['email']) or !isset($_POST['mensagem
 	try {
     //Server settings
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'mail.observatoriodesaudemental.com.br';                     //Set the SMTP server to send through
+    $mail->Host       = ***';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'contatoteste@observatoriodesaudemental.com.br';                     //SMTP username
-    $mail->Password   = '$Teste$Contato$22';                               //SMTP password
+    $mail->Password   = '**';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
